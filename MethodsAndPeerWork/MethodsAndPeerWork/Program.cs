e6689ead8883f2daf664b1935c0cefe8e72c2259
Lennon-0 <LennonEhare@Gmail.com>
2024-10-08 10:52:31 -0400

    added in most of my code required code and some guidelines for lydia
    I hope you don't find it degrading at all Lydia, just wanna help a little.

--- MethodsAndPeerWork/MethodsAndPeerWork/Program.cs
+++ MethodsAndPeerWork/MethodsAndPeerWork/Program.cs
@@ -5,22 +5,32 @@
      * Lydia Jin
      * IGME.201
      * due: Oct/13/2024
+     * be sure to look at instructons on mycourses 
      */
 
     // Lennon is partner A
     // Lydia is partner B
     internal class Program
     {
-        //Lennon's method
         
+        //Lennon's Add function  (i gotta use auto comment? whats that?)
+        static int Add(int num1, int num2) 
+        {//start Add
 
+            //Console.WriteLine("num1: " + num1 + "\tnum2: " + num2 ); //this is for testing and debuging purposes 
+            int sum = num1 + num2;
+            return (sum);
+        }//end Add
+
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!\n");
 
-            //varibles
-            int lennonFavoriteNumber;
-            int lydiaFavoriteNumber;
+            //varibles,
+            //Lennon has set them both to zero intially for the purpose of testing without the need to wait for lydia's code
+            int lennonFavoriteNumber = 0; 
+            int lydiaFavoriteNumber  = 0;
 
 
 
@@ -30,14 +40,18 @@
             //initialize your number lydia (replace this comment with your code)
 
 
+            //Lydia's Subtract function 
+
+
             //start of our actual program
             Console.WriteLine("Lennon's favorite number is " + lennonFavoriteNumber + "!");
             // write your favorite number in the console (replace this comment with your code)
 
 
-
-
-
+            int additiveSum = Add(lennonFavoriteNumber,lydiaFavoriteNumber);
+            Console.WriteLine("lennon's favorite number plus lydia's favorite number equals " + additiveSum);
+            // lydia calling their Subtract function (replace this comment with your code)  
+            //lydia's message to the console for their subract method (replace this comment with your code)  
 
         }//end of static void Main
     }//end of internal class
