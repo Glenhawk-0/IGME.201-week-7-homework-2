454aa01c1e4202b85aa17a12b116156736592d0c
Glenhawk-0 <LennonEhare@Gmail.com>
2024-10-08 09:06:03 -0400

    added int varibles
    lennonFavoriteNumber is initialized
    lydiaFavoriteNumber is not initialized

--- MethodsAndPeerWork/MethodsAndPeerWork/Program.cs
+++ MethodsAndPeerWork/MethodsAndPeerWork/Program.cs
@@ -1,10 +1,24 @@
 ﻿namespace MethodsAndPeerWork
 {
+    /*
+     * Lennon Hare
+     * Lydia Jin
+     * IGME.201
+     * due: Oct/13/2024
+     */
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-        }
-    }
-}
+
+            //varibles
+            int lennonFavoriteNumber = 83;
+            int lydiaFavoriteNumber; //add in your number lydia
+
+
+
+
+        }//end of static void Main
+    }//end of internal class
+}//end of namespace 
