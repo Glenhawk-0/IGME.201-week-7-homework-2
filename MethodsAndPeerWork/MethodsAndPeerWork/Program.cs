8b2933a215f81053092c1c4dbebc83a60046644e
Lennon-0 <LennonEhare@Gmail.com>
2024-10-08 10:26:37 -0400

    followed instructions right up to where partner's a and b are mentioned
    added placeholder comments for lydia

--- MethodsAndPeerWork/MethodsAndPeerWork/Program.cs
+++ MethodsAndPeerWork/MethodsAndPeerWork/Program.cs
@@ -6,15 +6,35 @@
      * IGME.201
      * due: Oct/13/2024
      */
+
+    // Lennon is partner A
+    // Lydia is partner B
     internal class Program
     {
+        //Lennon's method
+        
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
 
             //varibles
-            int lennonFavoriteNumber = 83;
-            int lydiaFavoriteNumber; //add in your number lydia
+            int lennonFavoriteNumber;
+            int lydiaFavoriteNumber;
+
+
+
+
+            //initialize
+            lennonFavoriteNumber = 83;
+            //initialize your number lydia (replace this comment with your code)
+
+
+            //start of our actual program
+            Console.WriteLine("Lennon's favorite number is " + lennonFavoriteNumber + "!");
+            // write your favorite number in the console (replace this comment with your code)
+
+
 
 
 
